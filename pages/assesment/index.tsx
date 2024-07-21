f8df8df5c14152f270a04053b64d3e1da419fa0a
Akshay Mattoo <akshaymattoo@gmail.com>
2024-07-20 23:30:40 -0700

    cosmetic changes

--- pages/assesment/index.tsx
+++ pages/assesment/index.tsx
@@ -6,33 +6,7 @@ import uiSchema from '../../schemas/uiSchema.json';
 import { materialRenderers } from '@jsonforms/material-renderers';
 import { useRouter } from 'next/router'
 import { Box, Button } from '@mui/material';
-import { styled } from '@mui/material/styles';
 
-const BootstrapButton = styled(Button)({
-    boxShadow: 'none',
-    textTransform: 'none',
-    fontSize: 16,
-    padding: '6px 12px',
-    border: '1px solid',
-    lineHeight: 1.5,
-    backgroundColor: 'black',
-    fontFamily: [
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-    ].join(','),
-    '&:hover': {
-        backgroundColor: 'black',
-    },
-
-});
 const index = () => {
     const [formData, setFormData] = useState({});
     const router = useRouter();
@@ -62,9 +36,9 @@ const index = () => {
                 renderers={materialRenderers}
                 onChange={({ data }) => setFormData(data)}
             />
-            <BootstrapButton variant="contained" disableRipple onClick={
+            <Button sx={{ color: 'white', backgroundColor: 'black' }} variant="contained" disableRipple onClick={
                 handleSubmit
-            } >Submit</BootstrapButton>
+            } >Submit</Button>
         </Box>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,8 @@
 import Head from "next/head";
 import { useRouter } from 'next/router'
 import { Box, Button, Stack, Typography } from "@mui/material";
-import { styled } from '@mui/material/styles';
 
 
-const BootstrapButton = styled(Button)({
-  boxShadow: 'none',
-  textTransform: 'none',
-  fontSize: 16,
-  padding: '6px 12px',
-  border: '1px solid',
-  lineHeight: 1.5,
-  backgroundColor: 'black',
-  fontFamily: [
-    '-apple-system',
-    'BlinkMacSystemFont',
-    '"Segoe UI"',
-    'Roboto',
-    '"Helvetica Neue"',
-    'Arial',
-    'sans-serif',
-    '"Apple Color Emoji"',
-    '"Segoe UI Emoji"',
-    '"Segoe UI Symbol"',
-  ].join(','),
-  '&:hover': {
-    backgroundColor: 'black',
-  },
-
-});
 export default function Home() {
   const router = useRouter();
 
@@ -40,18 +14,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box display="flex" justifyContent="center" alignItems="center" sx={{ minHeight: '100vh', backgroundColor: '#D9DEA5' }}>
-        <Stack spacing={3} textAlign="center" width="75%">
-          <Typography fontSize='6xl' width="100%">Immigration made easy!</Typography>
-          <Typography fontSize='2xl' width="65%" mx="auto">Alma simplifies immigration for technologists, founders, and researchers with our top legal talent and user-friendly platform.</Typography>
-          {/* <Button variant="contained" color="black"
-            _hover={{ bg: "gray.700" }}
-            borderRadius="50" width="30%" mx="auto" onClick={() => router.push('/assesment')}>Start assessment of your case!</Button> */}
+      <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" sx={{ widht: '100%', minHeight: '100vh', backgroundColor: '#D9DEA5' }}>
 
-          <BootstrapButton variant="contained" disableRipple onClick={() => router.push('/assesment')}>
-            Start assessment of your case!
-          </BootstrapButton>
-        </Stack>
+        <Typography fontSize={32} width="40%">Immigration made easy!</Typography>
+        <Typography fontSize={16} width="40%" mx="auto">Alma simplifies immigration for technologists, founders, and researchers with our top legal talent and user-friendly platform.</Typography>
+
+        <Button sx={{ width: '40%', color: 'white', backgroundColor: 'black' }} variant="contained" disableRipple onClick={() => router.push('/assesment')}>
+          Start assessment of your case!
+        </Button>
+
       </Box>
     </>
   );
